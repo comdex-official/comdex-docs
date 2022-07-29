3c458d1b7cbdcf817e4cb6472f3b0131a0f329a8
asif <asif.ansari7774@gmail.com>
2022-07-29 15:54:41 +0530

    update code

--- src/utils/SvgSpriteLoader.js
+++ src/utils/SvgSpriteLoader.js
@@ -6,7 +6,7 @@ const SvgSprite = (props) => {
 	const [isErrored, setIsErrored] = useState(false);
 
 	useEffect(() => {
-		let svgVersion = "1.0.8";
+		let svgVersion = "1.0.9";
 		console.log("SVG Sprite version : " + svgVersion);
 		var isLocalStorage =
 				"localStorage" in window && window["localStorage"] !== null,
