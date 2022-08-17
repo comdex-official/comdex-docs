fdac631bb52bc31a42c431e72400e2165be4882d
Dheeraj Dubey <dheerajkd3091@gmail.com>
2022-08-17 22:06:38 +0530

    Update index.js

--- src/components/layout/NavigationBar/index.js
+++ src/components/layout/NavigationBar/index.js
@@ -11,7 +11,7 @@ const NavigationBar = () => {
 					<SvgIcon name="logo" viewbox="0 0 112.095 24.926" />
 				</Link>
 			</div>
-			<Link className="right-link" to={{ pathname: 'https://github.com/comdex-official/docs' }} target="_black">
+			<Link className="right-link" to={{ pathname: 'https://github.com/comdex-official/comdex-docs' }} target="_black">
 				<SvgIcon name="core-icon" viewbox="0 0 34.356 34.355" />
 				Comdex Core
 			</Link>
