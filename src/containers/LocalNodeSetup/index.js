371ab93e045ad968ccac2e79b8ebc065102212ee
asif <asif.ansari7774@gmail.com>
2022-08-22 12:35:36 +0530

    update local node page content

--- src/containers/LocalNodeSetup/index.js
+++ src/containers/LocalNodeSetup/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BottomNav } from "../../components/common";
+import { Link } from "react-router-dom";
 import "./index.less";
 
 const LocalNodeSetup = () => {
@@ -7,8 +8,11 @@ const LocalNodeSetup = () => {
     <div>
       <h2>Local Node Setup</h2>
       <p>
-        ...
+        Please go through the steps mentioned below to set up the local net : 
       </p>
+      <Link to={{ pathname: 'https://github.com/comdex-official/comdex/blob/development/scripts/comdex_local_setup/README.md' }} target="_blank">
+        https://github.com/comdex-official/comdex/blob/development/scripts/comdex_local_setup/README.md
+      </Link>
 
       <BottomNav
         preNavLink="/using-generic-modules"
