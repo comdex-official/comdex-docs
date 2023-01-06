455ce25161d9d6664f40285e870091778d2de4df
Dheeraj Dubey <dheerajkd3091@gmail.com>
2023-01-06 19:50:05 +0530

    Update index.js

--- src/containers/JoincomdexMainNet/index.js
+++ src/containers/JoincomdexMainNet/index.js
@@ -12,7 +12,7 @@ const JoincomdexMainNet = () => {
       </p>
       <p>
         Main Net chain ID : comdex-1 <br />
-        Current Test Net Version : v6.0.2
+        Current Test Net Version : v7.0.0
       </p>
       <p>
         <Link to={{ pathname: 'https://github.com/comdex-official/networks/blob/main/mainnet/02-validator-post-gentx.md' }} target="_blank">https://github.com/comdex-official/networks/blob/main/mainnet/02-validator-post-gentx.md
