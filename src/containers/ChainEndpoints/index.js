a7cf98b15e7944ababf368f3115e53100f8cccf0
srinu391 <srinu@comdex.sg>
2023-02-22 15:07:51 +0530

    Update chain end points data mapping.

--- src/containers/ChainEndpoints/index.js
+++ src/containers/ChainEndpoints/index.js
@@ -1,47 +1,40 @@
-import { Table, Tag } from "antd";
+import { Table } from "antd";
 import React from "react";
 import { Link } from "react-router-dom";
-import { BottomNav, SvgIcon } from "../../components/common";
+import { BottomNav } from "../../components/common";
+import { chainEndPoints } from "./data";
 import "./index.less";
 
 const columns = [
   {
-    title: 'Chain Name',
-    dataIndex: 'chain_name',
-    key: 'chain_name'
+    title: "Chain Name",
+    dataIndex: "chain_name",
+    key: "chain_name",
   },
   {
-    title: 'RPC/REST Endpoints',
-    dataIndex: 'endpoints',
-    key: 'endpoints'
+    title: "RPC/REST Endpoints",
+    dataIndex: "endpoints",
+    key: "endpoints",
   },
   {
-    title: 'Name of the Validator',
-    dataIndex: 'nameof_validator',
-    key: 'nameof_validator'
+    title: "Name of the Validator",
+    dataIndex: "nameof_validator",
+    key: "nameof_validator",
   },
 ];
 
-const dataSource = [
-  {
-    key: '1',
-    chain_name: 'ATOM',
-    endpoints: <>comdex-rest.zenscape.one <br />  comdex-rpc.zenscape.one</>,
-    nameof_validator: 'axelar-dojo-1'
-  },
-  {
-    key: '2',
-    chain_name: 'OSMO',
-    endpoints: <>comdex-rest.zenscape.one <br /> comdex-rpc.zenscape.one</>,
-    nameof_validator: 'axelar-dojo-1'
-  },
-  {
-    key: '3',
-    chain_name: 'STRIDE',
-    endpoints: <>comdex-rest.zenscape.one <br />  comdex-rpc.zenscape.one</>,
-    nameof_validator: 'axelar-dojo-1'
-  },
-];
+const dataSource = chainEndPoints.map((item) => {
+  return {
+    key: item?.key,
+    chain_name: item?.chain_name,
+    endpoints: (
+      <>
+        {item?.rpc} <br /> {item?.rpc}
+      </>
+    ),
+    nameof_validator: item?.nameof_validator,
+  };
+});
 
 const ChainEndpoints = () => {
   return (
@@ -49,10 +42,24 @@ const ChainEndpoints = () => {
       <h2 className="mb-1">Comdex Chain Endpoints</h2>
       <h4>Please mention your RPC/REST endpoints for comdex products.</h4>
       <p>
-        <b>Note:</b> <span className="text-muted">Raise a PR on</span> <Link to={{ pathname: "https://github.com/comdex-official/comdex-docs/pulls" }} target="_blank">Github</Link>
+        <b>Note:</b> <span className="text-muted">Raise a PR on</span>{" "}
+        <Link
+          to={{
+            pathname: "https://github.com/comdex-official/comdex-docs/pulls",
+          }}
+          target="_blank"
+        >
+          Github
+        </Link>
       </p>
-      
-      <Table dataSource={dataSource} columns={columns} bordered pagination={false} scroll={{ y: 'calc(100vh - 480px)' }} />
+
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        bordered
+        pagination={false}
+        scroll={{ y: "calc(100vh - 480px)" }}
+      />
 
       <BottomNav
         preNavLink="/ibc-relayers"
@@ -60,7 +67,6 @@ const ChainEndpoints = () => {
         nextNavLink="/test-net"
         nextNavText="Join comdex Test Net as a validator"
       />
-
     </div>
   );
 };
