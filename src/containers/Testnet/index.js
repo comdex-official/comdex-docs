7456a1819195b8b1f2cd1a29576562f8d56c6186
srinu391 <srinu@comdex.sg>
2023-03-03 20:55:00 +0530

    Remove unused data.

--- src/containers/Testnet/index.js
+++ src/containers/Testnet/index.js
@@ -34,18 +34,6 @@ const columns = [
   },
 ];
 
-const dataSource = [
-  {
-    key: "1",
-    source_channel: "channel-51",
-    destination_chain: "Axelar",
-    destination_chainid: "axelar-dojo-1",
-    destination_channel: "channel-42",
-    known_operators:
-      "AUDIT.one, Cros-nest, Polkachu *looking for more operators*",
-  },
-];
-
 const Testnet = () => {
   return (
     <div>
