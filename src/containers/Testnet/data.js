597dfd23752054ec8379c99119f1128cc857dfc4
srinu391 <srinu@comdex.sg>
2023-03-03 18:57:35 +0530

    Add testnet data.

--- src/containers/Testnet/data.js
+++ src/containers/Testnet/data.js
@@ -1,11 +1,10 @@
 export const testnetIbcChannels = [
-    {
-      key: '1',
-      source_channel: 'channel-51',
-      destination_chain: 'Axelar',
-      destination_chainid: 'axelar-dojo-1',
-      destination_channel: 'channel-42',
-      known_operators: 'AUDIT.one, Cros-nest, Polkachu *looking for more operators*'
-    },
-  ];
-  
+  {
+    key: "1",
+    source_channel: "channel-4",
+    destination_chain: "Osmosis",
+    destination_chainid: "osmo-test-4",
+    destination_channel: "channel-1838",
+    known_operators: "",
+  },
+];
